06529c6301b80ddd20f7d79f74d3cd81aaabb0c0
SON YOUNGWOO <goldson12@naver.com>
2018-08-13 10:52:47 +0900

    love is timing

---
+++ src/loveistiming/Solution.java
@@ -0,0 +1,39 @@
+package loveistiming;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		int a = 11*24*60 + 11*60 + 11;
+		
+		if(t <= 10) {
+			
+			for(int i=1; i<=t; i++) {
+				int d = sc.nextInt();
+				int h = sc.nextInt();
+				int m = sc.nextInt();
+				
+				if(11<=d && d<=14 && 0<=h && h<=23 && 0<=m && m<=59) {
+					
+					int b = d*24*60 + h*60 + m;
+					int result = 0;
+					
+					if(a > b) result = -1;
+					else if(a == b) result = 0;
+					else result = b-a;
+					
+					System.out.println("#"+i+" "+result);
+				}
+				
+			}
+			
+		}
+		
+	}
+
+}
